f949b5bf80c0e516ba5d4a47075bcaf5fb2d4a67
Julian Berman <Julian@GrayVines.com>
2024-10-31 10:19:22 -0400

    Drop support for 3.8, which is EOL.

--- noxfile.py
+++ noxfile.py
@@ -17,8 +17,8 @@ REQUIREMENTS_IN = [  # this is actually ordered, as files depend on each other
     (path.parent / f"{path.stem}.in", path) for path in REQUIREMENTS.values()
 ]
 
-SUPPORTED = ["3.8", "3.9", "3.10", "pypy3.10", "3.11", "3.12", "3.13"]
-LATEST = "3.12"
+SUPPORTED = ["3.9", "3.10", "pypy3.10", "3.11", "3.12", "3.13"]
+LATEST = "3.13"
 
 nox.options.default_venv_backend = "uv|virtualenv"
 nox.options.sessions = []
